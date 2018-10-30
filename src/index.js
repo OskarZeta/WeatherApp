7906113f87b9782c768f9ab06bab8f89a4244a48
Li Dian <dw4h@yandex.ru>
2018-10-30 14:17:12 +0300

    added some functions and 404 page for correct routing

--- src/index.js
+++ src/index.js
@@ -4,16 +4,13 @@ import { BrowserRouter } from 'react-router-dom';
 import './css/bootstrap.css';
 import './css/style.css';
 import App from './Components/App';
-import * as serviceWorker from './serviceWorker';
+//import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
   </BrowserRouter>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//serviceWorker.unregister();
